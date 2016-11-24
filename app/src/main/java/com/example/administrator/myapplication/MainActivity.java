78c8050d25a433e1cefbbcddfeb2f8059edcbfbd
androidboom <525114757@qq.com>
2016-11-24 14:37:01 +0800

    实验刘博

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //更新
+    //实验
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
